67f09ae05d1e1c7ee64b43adf902a4f604e2f59c
John Hammerlund <johnhammerlund@gmail.com>
2016-02-18 13:54:47 -0800

    Add framework project (no source) and update .gitignore

---
+++ Kava/Kava/Kava.h
@@ -0,0 +1,16 @@
+//
+//  Kava.h
+//  Kava
+//
+//  Created by John Hammerlund on 2/18/16.
+//  Copyright © 2016 MINDBODY, Inc. All rights reserved.
+//
+
+@import Foundation
+
+//! Project version number for Kava.
+FOUNDATION_EXPORT double KavaVersionNumber;
+
+//! Project version string for Kava.
+FOUNDATION_EXPORT const unsigned char KavaVersionString[];
+
